51d759279f78b9782f20161ae4064585473b236b
Ondřej Vorlíček <vorlicek@diamo.cz>
2024-05-10 08:23:50 +0200

    Povýšení SB na 3.2.5.

--- vratnice/src/main/java/cz/diamo/share/repository/ZastupRepository.java
+++ vratnice/src/main/java/cz/diamo/share/repository/ZastupRepository.java
@@ -17,6 +17,6 @@ public interface ZastupRepository extends JpaRepository<Zastup, String> {
 	@Query("select case when  count(s) > 0 then true else false end from Zastup s where s.guid != :guid and s.uzivatel.idUzivatel = :idUzivatel and s.uzivatelZastupce.idUzivatel = :idUzivatelZastupce and s.aktivita = true and s.platnostOd <= :datumDo and s.platnostDo >= :datumOd")
 	boolean exists(String guid, String idUzivatel, String idUzivatelZastupce, Date datumOd, Date datumDo);
 
-	@Query("select case when count(zastup) > 0 then true else false end from Zastup zastup where zastup.uzivatelZastupce.idUzivatel = :idUzivatel and zastup.uzivatel.idUzivatel = :idZastup and zastup.platnostOd <= now() and zastup.platnostDo >= now() and zastup.aktivita = true")
+	@Query("select case when count(zastup) > 0 then true else false end from Zastup zastup where zastup.uzivatelZastupce.idUzivatel = :idUzivatel and zastup.uzivatel.idUzivatel = :idZastup and zastup.platnostOd <= CURRENT_TIMESTAMP() and zastup.platnostDo >= CURRENT_TIMESTAMP() and zastup.aktivita = true")
 	boolean platnyZastup(String idUzivatel, String idZastup);
 }
