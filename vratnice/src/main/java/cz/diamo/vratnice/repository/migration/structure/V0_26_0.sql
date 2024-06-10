fb4c8a294917dd2fcb1d81b076d44d8d8abfcbd8
o_kudela <kudela@diamo.cz>
2024-06-10 14:43:46 +0200

    přidání rolí

---
+++ vratnice/src/main/java/cz/diamo/vratnice/repository/migration/structure/V0_26_0.sql
@@ -0,0 +1,8 @@
+insert into vratnice.zdrojovy_text (hash, text) values ('ROLE_SPRAVA_ZADOSTI_KLICU','Správa žádostí klíčů');
+insert into vratnice.role (authority, nazev_resx) values ('ROLE_SPRAVA_ZADOSTI_KLICU', 'ROLE_SPRAVA_ZADOSTI_KLICU');
+
+insert into vratnice.zdrojovy_text (hash, text) values ('ROLE_SPRAVA_VYPUJCEK_KLICU','Správa výpůjček klíčů');
+insert into vratnice.role (authority, nazev_resx) values ('ROLE_SPRAVA_VYPUJCEK_KLICU', 'ROLE_SPRAVA_VYPUJCEK_KLICU');
+
+insert into vratnice.zdrojovy_text (hash, text) values ('ROLE_ZADATEL_KLICE','Žadatel o klíč');
+insert into vratnice.role (authority, nazev_resx) values ('ROLE_ZADATEL_KLICE', 'ROLE_ZADATEL_KLICE');
