77e5a42bd373cb9c914f0681b48c148f9e8091d9
o_kudela <kudela@diamo.cz>
2024-07-04 10:31:43 +0200

    Vytvoření Controlleru pro konfiguraci vrátnice

---
+++ vratnice/src/main/java/cz/diamo/vratnice/controller/VratniceKonfiguraceController.java
@@ -0,0 +1,38 @@
+package cz.diamo.vratnice.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import cz.diamo.share.controller.BaseController;
+import cz.diamo.vratnice.configuration.VratniceProperties;
+import cz.diamo.vratnice.dto.VratniceKonfiguraceDto;
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class VratniceKonfiguraceController extends BaseController {
+
+    final static Logger logger = LogManager.getLogger(VratniceKonfiguraceController.class);
+
+    @Autowired
+    private VratniceProperties vratniceProperties;
+
+    @GetMapping("/vratnice-konfigurace")
+    public VratniceKonfiguraceDto detail(HttpServletRequest request) {
+        VratniceKonfiguraceDto vratniceKonfiguraceDto = new VratniceKonfiguraceDto();
+
+        vratniceKonfiguraceDto.setZavodNazev(vratniceProperties.getZavodNazev());
+        vratniceKonfiguraceDto.setDobaPouceni(vratniceProperties.getDobaPouceni());
+        vratniceKonfiguraceDto.setBarvaVyprseniPouceni(vratniceProperties.getBarvaVyprseniPouceni());
+        vratniceKonfiguraceDto.setBarvaPovoleneSpz(vratniceProperties.getBarvaPovoleneSpz());
+        vratniceKonfiguraceDto.setBarvaNepovoleneSpz(vratniceProperties.getBarvaNepovoleneSpz());
+
+        return vratniceKonfiguraceDto;
+    }
+    
+
+}
---
+++ vratnice/src/main/java/cz/diamo/vratnice/dto/VratniceKonfiguraceDto.java
@@ -0,0 +1,24 @@
+package cz.diamo.vratnice.dto;
+
+import java.io.Serializable;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class VratniceKonfiguraceDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String zavodNazev;
+
+    private Integer dobaPouceni;
+
+    private String barvaVyprseniPouceni;
+
+    private String barvaNepovoleneSpz;
+
+    private String barvaPovoleneSpz;
+
+}
