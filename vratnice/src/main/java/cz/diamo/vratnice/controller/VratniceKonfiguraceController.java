dd306726e3ed08095ecd0d7b00e3e4b7ab543951
o_kudela <kudela@diamo.cz>
2024-08-08 10:18:15 +0200

    Implementace validace platnosti datumu poučení BOZP

--- vratnice/src/main/java/cz/diamo/vratnice/controller/VratniceKonfiguraceController.java
+++ vratnice/src/main/java/cz/diamo/vratnice/controller/VratniceKonfiguraceController.java
@@ -1,8 +1,13 @@
 package cz.diamo.vratnice.controller;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import cz.diamo.share.controller.BaseController;
@@ -11,6 +16,9 @@ import cz.diamo.vratnice.dto.VratniceKonfiguraceDto;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -34,6 +42,25 @@ public class VratniceKonfiguraceController extends BaseController {
 
         return vratniceKonfiguraceDto;
     }
-    
 
+@PostMapping("/vratnice-konfigurace/is-datum-pouceni-valid")
+    public ResponseEntity<Boolean> isDatumPouceniValid(
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date datumPouceni) {
+        
+        int dobaPouceni = vratniceProperties.getDobaPouceni();
+
+        // Získání aktuálního data
+        Calendar cal = Calendar.getInstance();
+        Date currentDate = cal.getTime();
+
+        // Přidání dobaPouceni měsíců k datumPouceni
+        cal.setTime(datumPouceni);
+        cal.add(Calendar.MONTH, dobaPouceni);
+        Date datumPouceniPlusDoba = cal.getTime();
+
+        // Porovnání s aktuálním datem
+        boolean isValid = datumPouceniPlusDoba.after(currentDate);
+
+        return ResponseEntity.ok(isValid);
+    }
 }
