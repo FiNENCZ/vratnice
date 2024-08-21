1a3cc661bae6c03a23c63f460f61660c56afbabf
vorlicek <vorlicek@diamo.cz>
2024-08-21 06:52:31 +0200

    Oprava scriptu.

--- vratnice/src/main/java/cz/diamo/vratnice/repository/migration/structure/V0_94_0.sql
+++ vratnice/src/main/java/cz/diamo/vratnice/repository/migration/structure/V0_94_0.sql
@@ -1,5 +1,13 @@
+-- Přejmenování správy externích uživatelů na správu externích systémů
+update vratnice.zdrojovy_text set text = 'Správa externích systémů' where hash = 'ROLE_SPRAVA_EXTERNICH_UZIVATELU';
+
 -- Doplněn příznak externího uživatele
 ALTER TABLE vratnice.uzivatel ADD externi boolean not null default false;
+
+-- Nová role pro správu vytváření nových externích uživatelů
+insert into vratnice.zdrojovy_text (hash, text) values ('ROLE_SPRAVA_UZIVATELU_EXT','Správce externích uživatelů');
+insert into vratnice.role (authority, nazev_resx) values ('ROLE_SPRAVA_UZIVATELU_EXT', 'ROLE_SPRAVA_UZIVATELU_EXT');
+
 
 -- Verze DB
 update vratnice.databaze set verze_db = 0, sub_verze_db = 94, cas_zmn = now(), zmenu_provedl = 'pgadmin';
