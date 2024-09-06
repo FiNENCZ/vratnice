78852f25b086a783c3dd1bbc64f83549b34dd150
Jan Pickar <pickar@diamo.cz>
2024-09-06 08:34:00 +0200

    Po synchronizaci s master

--- vratnice/src/main/java/cz/diamo/vratnice/repository/migration/structure/V0_96_0.sql
+++ vratnice/src/main/java/cz/diamo/vratnice/repository/migration/structure/V0_96_0.sql
@@ -1,9 +1,23 @@
-update vratnice.lokalita set id_externi = id_lokalita, cas_zmn = now(), zmenu_provedl = 'pgadmin' where id_externi is null;
+-- Tabulka stavu žádosti
+create table vratnice.zadost_stav (
+     id_zadost_stav INTEGER not null,
+     nazev_resx VARCHAR(100) not null,
+    constraint pk_zadost_stav primary key (id_zadost_stav)            
+);
 
-ALTER TABLE vratnice.lokalita RENAME COLUMN id_externi TO kod;
-ALTER TABLE vratnice.lokalita ALTER COLUMN kod TYPE varchar(100) USING kod::varchar(100);
-ALTER TABLE vratnice.lokalita ALTER COLUMN kod SET NOT NULL;
-ALTER TABLE vratnice.lokalita ALTER COLUMN nazev TYPE varchar(1000) USING nazev::varchar(1000);
+insert into vratnice.zdrojovy_text (hash, text) values ('STAV_ZADOST_SCHVALENO','Schváleno');
+insert into vratnice.zadost_stav (id_zadost_stav, nazev_resx) values (1, 'STAV_ZADOST_SCHVALENO');
+insert into vratnice.zdrojovy_text (hash, text) values ('STAV_ZADOST_POZASTAVENO','Pozastaveno');
+insert into vratnice.zadost_stav (id_zadost_stav, nazev_resx) values (2, 'STAV_ZADOST_POZASTAVENO');
+insert into vratnice.zdrojovy_text (hash, text) values ('STAV_ZADOST_UKONCENO','Ukončeno');
+insert into vratnice.zadost_stav (id_zadost_stav, nazev_resx) values (3, 'STAV_ZADOST_UKONCENO');
+
+ALTER TABLE vratnice.zadost_klic RENAME COLUMN stav TO id_zadost_stav;
+ALTER TABLE vratnice.zadost_klic ALTER COLUMN id_zadost_stav TYPE integer USING id_zadost_stav::integer;
+
+ALTER TABLE vratnice.zadost_klic ADD CONSTRAINT fk_zadost_id_zadost_stav
+	FOREIGN KEY (id_zadost_stav) REFERENCES vratnice.zadost_stav (id_zadost_stav) ON DELETE No Action ON UPDATE No Action;
+
 
 -- Verze DB
 update vratnice.databaze set verze_db = 0, sub_verze_db = 96, cas_zmn = now(), zmenu_provedl = 'pgadmin';
---
+++ vratnice/src/main/java/cz/diamo/vratnice/repository/migration/structure/V0_97_0.sql
@@ -0,0 +1,9 @@
+update vratnice.lokalita set id_externi = id_lokalita, cas_zmn = now(), zmenu_provedl = 'pgadmin' where id_externi is null;
+
+ALTER TABLE vratnice.lokalita RENAME COLUMN id_externi TO kod;
+ALTER TABLE vratnice.lokalita ALTER COLUMN kod TYPE varchar(100) USING kod::varchar(100);
+ALTER TABLE vratnice.lokalita ALTER COLUMN kod SET NOT NULL;
+ALTER TABLE vratnice.lokalita ALTER COLUMN nazev TYPE varchar(1000) USING nazev::varchar(1000);
+
+-- Verze DB
+update vratnice.databaze set verze_db = 0, sub_verze_db = 97, cas_zmn = now(), zmenu_provedl = 'pgadmin';
