80be1596bd54172a0d77e30cf6f2e1cff898fa79
Jan Pickar <pickar@diamo.cz>
2024-09-06 08:12:02 +0200

    Upravy

--- vratnice/src/main/java/cz/diamo/vratnice/repository/migration/structure/V0_96_0.sql
+++ vratnice/src/main/java/cz/diamo/vratnice/repository/migration/structure/V0_96_0.sql
@@ -1,3 +1,5 @@
+update vratnice.lokalita set id_externi = id_lokalita, cas_zmn = now(), zmenu_provedl = 'pgadmin' where id_externi is null;
+
 ALTER TABLE vratnice.lokalita RENAME COLUMN id_externi TO kod;
 ALTER TABLE vratnice.lokalita ALTER COLUMN kod TYPE varchar(100) USING kod::varchar(100);
 ALTER TABLE vratnice.lokalita ALTER COLUMN kod SET NOT NULL;
